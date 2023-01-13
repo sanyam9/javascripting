28dcf74d4507f7881aae756ba646fd32d73a5a2f
sanyam9 <sanyamxsharma@gmail.com>
2023-01-13 15:55:46 +0530

    feat: add arrow functions

--- primeAndArmstrong.js
+++ primeAndArmstrong.js
@@ -1,25 +1,51 @@
-function isPrime(n){
-    for(let i = 2; i <=Math.pow(n,0.5) ; i++){
-        if(n%i==0){
+function isPrime(num){
+    for(let i = 2; i <=Math.pow(num,0.5) ; i++){
+        if(num%i==0){
             return false
         }
     }
     return true
 }
 
-function isArmstrong(n){
-    let digits = String(n).length
+
+function isArmstrong(num){
+    let digits = String(num).length
     let sum = 0
-    let temp = n
+    let temp = num
     while(temp!=0){
         sum = sum + Math.pow(temp%10,digits)
         temp = parseInt(temp/10)
     }
-    if(sum===n)
+    if(sum===num)
         return true
     else   
         return false;
 
 }
-console.log(isPrime(29))
-console.log(isArmstrong(92727))
+
+let isPrimeArrow = (num) => {
+    for(let i = 2; i <=Math.pow(num,0.5) ; i++){
+        if(num%i==0){
+            return false
+        }
+    }
+    return true
+}
+
+
+let isArmstrongArrow = (num) => {
+    let digits = String(num).length
+    let sum = 0
+    let temp = num
+    while(temp!=0){
+        sum = sum + Math.pow(temp%10,digits)
+        temp = parseInt(temp/10)
+    }
+    if(sum===num)
+        return true
+    else   
+        return false;
+
+}
+console.log(isPrimeArrow(29))
+console.log(isArmstrongArrow(92727))
