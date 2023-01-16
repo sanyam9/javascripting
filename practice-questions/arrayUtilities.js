64ba78d034f565600caed00ad286d865a9cfbfa8
sanyam9 <sanyamxsharma@gmail.com>
2023-01-16 23:35:04 +0530

    chore: add functionality to get even tripled elements using reduce

--- practice-questions/arrayUtilities.js
+++ practice-questions/arrayUtilities.js
@@ -6,13 +6,23 @@ const doubleNumbers = (numbers) => {
 }
 
 const getEvenTripledNumbers = (numbers) => {
-    if (!Array.isArray(numbers)){
+    if (!Array.isArray(numbers)) {
         throw new Error('Unsupported Input Type')
     }
-    if(numbers.some(isNaN)){
+    if (numbers.some(isNaN)) {
         throw new Error('Array should only contain numbers')
     }
-    return numbers.map(num => num*3).filter(num => num%2===0)
+    
+    //return numbers.map(num => num*3).filter(num => num%2===0)
+    
+    // return numbers.reduce((acc, curr) => {
+    //     if (curr % 2 === 0){
+    //         acc.push(3*curr)
+    //     }
+    //     return acc
+    // }, [])
+    
+    return numbers.reduce((acc,curr) => curr%2===0 ? [...acc,curr*3] : acc,[])
 }
-//console.log(getEvenTripledNumbers(['']))
-module.exports = { doubleNumbers, getEvenTripledNumbers}
+console.log(getEvenTripledNumbers([1,2,3,4,5,6,7,8,9]))
+module.exports = { doubleNumbers, getEvenTripledNumbers }
